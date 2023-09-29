498c5f074a3d3b343ae991f31b30e9180668960a
Rafael Cavalcanti <145015269+rafaelc-13@users.noreply.github.com>
2023-09-29 07:50:04 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -3,5 +3,12 @@ uma pessoa e diga em qual ano ela
 nasceu'''
 
 n = int(input("Digite sua idade: "))
+mes_idade = int(input('Em qu mês voce nasceu (1-12)? '))
+mes_atual = int(input('Em qu mês do ano estamos (1-12)? '))
 IDADE = 2023 - n
-print(f'Sua idade é {IDADE}.')
+
+if mes_idade >= mes_atual:
+    IDADE -=1
+
+print(f'Você nasceu em {IDADE}.')
+
